799541302df4586f9605708db4e9cebb2ab58494
Zyperr <ramerica200@gmail.com>
2024-10-06 18:04:26 -0300

    Se creo funcion para obtener informacion adicional de los capitulos

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 import { launch } from "puppeteer";
 import path from "path";
 import { readdir } from "fs/promises";
-
+import { readFile,writeFile } from "fs/promises";
 const folderPath = "./public/imgs";
 let maxVolumes = 109;
 let countVolumes = 0;
@@ -116,6 +116,69 @@ async function readFolder() {
     return f
 }
 
+async function getChaptersInfo(domain) {
+    try {
+        console.log("Launching browser")
+        let timea = 10000 * (1110-999) 
+        const browser = await launch({
+            headless: true,
+            protocolTimeout: timea,
+        });
+        console.log("New page")
+        const page = await browser.newPage();
+        console.log("Setting navigation timeout")
+        page.setDefaultNavigationTimeout(timea);
+        console.log("Going to domain")
+       
+
+        console.log("Evaluating page")
+
+        let i = 999;
+
+        while (i <= 1110) {
+            await page.goto(`${domain}/wiki/Chapter_${i}`);
+
+            const summaryChapter = await page.evaluate(() => {
+                return document.querySelector("#Short_Summary")?.parentElement?.nextElementSibling?.textContent
+            })
+            const coverPageInfo = await page.evaluate(() => {
+                return document.querySelector("#Cover_Page")?.parentElement.nextElementSibling?.textContent
+            })
+            const coverImage = await page.evaluate(() => {
+                return document.querySelector(".image")?.childNodes[1]?.src
+            })
+
+            
+            console.log("reading json")
+            const dataJson = await readFile('./data.json', 'utf-8');
+            console.log("Parsing json")
+            const dataParse = JSON.parse(dataJson);
+            console.log("Pushing data to parsed json")
+            dataParse.Chapters.forEach((chapter) => {
+                if (chapter.Chapter == i) {
+                    chapter.Summary = summaryChapter
+                    chapter.CoverPageInfo = coverPageInfo
+                    chapter.Cover = coverImage
+                }
+
+            })
+            console.log("Converting json to string")
+            const newData = JSON.stringify(dataParse, null, 2);
+            console.log("Writing json")
+            try {
+                await writeFile('data.json', newData, 'utf-8');
+            } catch (e) {
+                console.log(e)
+            }
+            console.log("Going back to list of chapters")
+            i++;
+        }
+        console.log("Closing browser")
+        await browser.close();
+    } catch (e) {
+        console.log(e)
+    }
+}
 function pagination(data, page = 1, perPage = 10) {
     if (perPage > 10) {
         throw new Error("Maximun perPage is 10")
@@ -129,9 +192,9 @@ function pagination(data, page = 1, perPage = 10) {
     return {
         elements,
         pages,
-        itemPerPage:perPage,
+        itemPerPage: perPage,
         totalItems: data.length,
-        currentPage:page
+        currentPage: page
     }
 }
 
@@ -140,5 +203,6 @@ function pagination(data, page = 1, perPage = 10) {
 export {
     openPageToGetImgs,
     readFolder,
-    pagination
+    pagination,
+    getChaptersInfo
 }
